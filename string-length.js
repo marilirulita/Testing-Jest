72154d3f7f6451f20dec1d1602fafe1418b20978
marysol <marilirulita@gmail.com>
2022-01-19 09:14:23 -0800

    Add string lengt requirements in function

--- string-length.js
+++ string-length.js
@@ -1,3 +1,8 @@
-const stringLength = str => str.length;
+const stringLength = (string) => {
+  if (string.length > 10 || string.length < 1) {
+    throw new Error("string is not lenght permit");
+  }
+  return string.length;
+}
 
 module.exports = stringLength;
--- string.test.js
+++ string.test.js
@@ -11,3 +11,15 @@ test('string lenght iqual 12', () => {
 test('string lenght iqual 3', () => {
   expect(stringLength(relative)).toBe(3);
 });
+
+test('string lenght is allowed (biger)', () => {
+  expect(stringLength(relative)).toBeGreaterThan(1);
+});
+
+test('string lenght is allowed (smaller)', () => {
+  expect(stringLength(username)).toBeLessThan(10);
+});
+
+test('string length not allowed', () => {
+  expect(() => stringLength(longUser)).toThrow(Error);
+});
