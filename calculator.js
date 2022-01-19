eaec629db6d1434905dd439c94fa9fb94b37659d
marysol <marilirulita@gmail.com>
2022-01-19 09:22:24 -0800

    Add calculator class and tests

---
+++ calculator.js
@@ -0,0 +1,25 @@
+class Calculator {
+  constructor(a, b) {
+    this.a = a;
+    this.b = b;
+  }
+ 
+  add () {
+    const answ = this.a + this.b;
+    return answ;
+  }
+  
+  substract () {
+    return this.a - this.b;
+  }
+
+  divide () {
+    return this.a / this.b;
+  }
+
+  multiply () {
+    return this.a * this.b;
+  }
+}
+
+module.exports = Calculator;
